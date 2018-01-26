4a587de364564ac38548028e4d54df6fc2a6fe71
FatihOzkaya18 <34345364+FatihOzkaya18@users.noreply.github.com>
2018-01-26 17:31:50 +0200

    Add files via upload

---
+++ Fatih Özkaya User Login Projesi/Login Ekranı/Login Ekranı/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Login_Ekranı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "------------------PLEASE LOG IN------------------\r\n";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLogin.Location = new System.Drawing.Point(102, 173);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(121, 48);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Log In";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(27, 83);
+            this.txtUserName.MaxLength = 20;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(285, 20);
+            this.txtUserName.TabIndex = 4;
+            this.txtUserName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUserName_KeyPress);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(27, 128);
+            this.txtPassword.MaxLength = 8;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(285, 20);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(27, 64);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 241);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "User Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ Fatih Özkaya User Login Projesi/Login Ekranı/Login Ekranı/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Login_Ekranı.Data;
+using System.Data.Entity;
+
+namespace Login_Ekranı
+{
+    public partial class Form1 : Form
+    {
+        LoginDbEntities db = new LoginDbEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int loginError = 0;
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == "" && txtPassword.Text == "")
+            {
+                MessageBox.Show("Kullanıcı Adı ve Şifre Boş Bırakılamaz.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (txtUserName.Text == "" && txtPassword.Text != "")
+            {
+                MessageBox.Show("Kullanıcı Adı Boş Bırakılamaz.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (txtUserName.Text != "" && txtPassword.Text == "")
+            {
+                MessageBox.Show("Şifre Boş Bırakılamaz.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (txtUserName.Text.Length < 2)//MaxLength 20 properties kısmından belirlenmiştir.
+            {
+                MessageBox.Show("Username bilgisini giriniz.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //4 - Password alanına her türlü string ifade girilebilir.En fazla 8 en az 5 karakter olmalıdır. 5 karakterden az bilgi girildiğinde "Password bilgisini giriniz" mesajı görüntülenecek
+            else if (txtPassword.Text.Length < 5)//MaxLength 8 properties kısmından belirlenmiştir.
+            {
+                MessageBox.Show("Password bilgisini giriniz.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            var kullanici = db.Uyeler.Where(x => x.kullanici_adi == txtUserName.Text).FirstOrDefault();
+
+            if (kullanici == null)
+            {
+                lblMessage.Text = $"'{txtUserName.Text}' Sistemde Bulunmadı";
+
+            }
+
+            else
+            {
+                kullanici = db.Uyeler.Where(x => x.kullanici_adi == txtUserName.Text && x.kullanici_sifre == txtPassword.Text && x.IsActive == true).FirstOrDefault();
+                if (kullanici == null)
+                {
+                    if (loginError < 3)
+                    {
+                        loginError++;
+                    }
+                    else//3'ten fazla girilirse hesap bloke edilecektir.
+                    {
+                        MessageBox.Show("Hesap Bloke Edilmiştir", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        var uyeBloke = db.Uyeler.Where(x => x.kullanici_adi == txtUserName.Text).FirstOrDefault();
+                        uyeBloke.IsActive = false;
+                        db.Entry(uyeBloke).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Giriş Başarılı", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+        private void txtUserName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //3- UserName alanına harf ve rakam girilebilir ama noktalama işareti girilemez. En fazla 20 en az 2 karakter olmalıdr. 2 karakterden az bilgi girildiğinde "Username bilgisini giriniz" mesajı görüntülenecek.
+            e.Handled = Char.IsPunctuation(e.KeyChar);//Textboxta özel karakter girilmesini önler.
+        }
+    }
+}
